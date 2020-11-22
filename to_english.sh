dfaf45a3fc2de7e413d0af781f2f0d8b8993a737
Tomoki Murayama <tomoki@TomokinoMac-mini.local>
2020-11-22 18:31:39 +0900

    first commit

---
+++ to_english.sh
@@ -0,0 +1,8 @@
+array=("Applications" "Downloads" "Pictures" "Public" "Movies" "Music" "Desktop")
+
+for i in "${array[@]}"
+do
+rm "$HOME/${i}/.localized"
+echo "Applyed to ${i}"
+done
+echo "complete!"
---
+++ to_japanese.sh
@@ -0,0 +1,8 @@
+array=("Applications" "Downloads" "Pictures" "Public" "Movies" "Music" "Desktop")
+
+for i in "${array[@]}"
+do
+touch "$HOME/${i}/.localized"
+echo "Applyed to ${i}"
+done
+echo "complete!"
